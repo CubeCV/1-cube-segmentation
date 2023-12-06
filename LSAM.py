907a29013ec34d62f3c81e9e7a3b47989594d691
Tim Xie <pdt.xie@gmail.com>
2023-12-05 21:23:21 -0800

    rebase clean up

--- LSAM.py
+++ LSAM.py
@@ -4,9 +4,9 @@ import supervision as sv
 import numpy as np
 
 model = LangSAM()
-image_pil = Image.open("cube_pics/IMG_9779.jpg").convert("RGB")
+image_pil = Image.open("original/1.jpg").convert("RGB")
 image_pil = image_pil.resize((300, 300))
-text_prompt = "Rubiks Cube"
+text_prompt = "Rubik's Cube"
 # sv.plot_image(np.array(image_pil), size=(16,16))
 masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
 
@@ -16,12 +16,10 @@ masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
 #     grid_size=(8, 2),
 #     size=(16, 16)
 # )
+
 for i in range(image_pil.size[0]):
     for j in range(image_pil.size[1]):
         if not masks[0][j][i] == 1:
-            image_pil.putpixel((i,j), (0,0,0))
+            image_pil.putpixel((i, j), (0, 0, 0))
 
-sv.plot_image(np.array(image_pil), size=(16,16))
-
-
-
+sv.plot_image(np.array(image_pil), size=(16, 16))
--- mSAM.py
+++ mSAM.py
@@ -1,12 +1,18 @@
-from mobile_sam import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-from groundingdino.util.inference import load_model, load_image, predict, annotate
 import cv2
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 from torchvision.ops import box_convert
+from PIL import Image
+from mobile_sam import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+from groundingdino.util.inference import load_model, load_image, predict, annotate
+
 
 model = load_model("GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "GroundingDINO/weights/groundingdino_swint_ogc.pth", device='cpu')
 img_num = 5
-IMAGE_PATH = f"cube_pics/{img_num}.jpg"
+
+IMAGE_PATH = f"original/{img_num}.jpg"
 TEXT_PROMPT = "Rubik's Cube"
 BOX_TRESHOLD = 0.35
 TEXT_TRESHOLD = 0.25
@@ -27,9 +33,6 @@ print("DINO INF: ", time.time() - start)
 xyxy = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()[0]
 print("xywh", xyxy)
 
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
 
 model_type = "vit_t"
 sam_checkpoint = "./MobileSAM/weights/mobile_sam.pt"
@@ -61,10 +64,11 @@ print("mSAM INF:", time.time() - start)
 for i in range(image_pil.size[0]):
     for j in range(image_pil.size[1]):
         if not masks[0][j][i] == 1:
-            image_pil.putpixel((i,j), (0,0,0))
+            image_pil.putpixel((i, j), (0, 0, 0))
+
 
 def show_mask(mask, ax):
-    color = np.array([30/255, 144/255, 255/255, 0.6])
+    color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
@@ -74,15 +78,15 @@ annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits
 cv2.imwrite("annotated_image.jpg", annotated_frame)
 
 
-f, axarr = plt.subplots(2+len(masks),1)
+f, axarr = plt.subplots(2 + len(masks), 1)
 
-image_pil.save(f"isolated_cubes/isolated_cube{img_num}.jpg")
+image_pil.save(f"isolated/{img_num}.jpg")
 
 axarr[0].imshow(image_pil)
 axarr[1].imshow(annotated_frame)
 
 for i, mask in enumerate(masks):
-    show_mask(mask, axarr[i+2])
+    show_mask(mask, axarr[i + 2])
 
 print("masks number: ", len(masks))
 
